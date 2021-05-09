f628294f03a3b61931404799fcdff0d6844a7e9e
pveda1 <parulveda1@gmail.com>
2021-05-08 17:36:13 -0700

    initial commit

---
+++ ClimateChangeApp/ClimateChangeApp/ClimateChangeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimateChangeAppApp.swift
+//  ClimateChangeApp
+//
+//  Created by Parul Veda on 5/8/21.
+//
+
+import SwiftUI
+
+@main
+struct ClimateChangeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
